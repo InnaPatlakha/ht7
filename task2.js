3b0310100aa4c7229eff4a310ec0ab6bba5f8239
Inna Patlakha <i.inna1903@gmail.com>
2023-07-20 23:32:17 +0300

    task2 done

--- task2.js
+++ task2.js
@@ -1,1 +1,7 @@
 // Розбити за цифрами п'ятизначне число і вивести у вихідному порядку через пробіл.
+
+let digit = String(prompt ("Введіть п'ятизначне число"));
+let arr1 = digit.split("");
+let arr2 = arr1.reverse();
+let result = arr2.join(' ');
+console.log (result);
